02dad3c2159880bee9ac332a8e1def25b13b17ef
Raegan Jarvis <raeganejarvis@gmail.com>
2018-05-15 13:55:50 -0700

    Code to select useful columns from dataset

--- Scripts/Week8_ReefFish.R
+++ Scripts/Week8_ReefFish.R
@@ -2,3 +2,10 @@
 
 reef_fish <- read.csv("Data/Supplemental201.csv")
 View(reef_fish)
+
+# Isolate useful columns
+
+library(tidyverse)
+
+reef_fish_bootless <- select(reef_fish, Genus, species, Bootless, Museum.Bootless, Fished)
+View(reef_fish_bootless)
