74c2138bf96aee3ca310f545055b7d7c06499ae1
sr320 <roberts.sbr@gmail.com>
2018-05-16 11:19:12 -0700

    add code to summarize fish based on Genus.

--- Scripts/Week8_ReefFish.R
+++ Scripts/Week8_ReefFish.R
@@ -9,3 +9,15 @@ library(tidyverse)
 
 reef_fish_bootless <- select(reef_fish, Genus, species, Bootless, Museum.Bootless, Fished)
 View(reef_fish_bootless)
+
+
+
+
+#count fish at each site based on genus, listing number of species
+group_by(reef_fish_bootless, Genus) %>%
+  summarise(
+    bootless = sum(Bootless),
+    Museum.Bootless = sum(Museum.Bootless),
+    n_species = n())
+
+
