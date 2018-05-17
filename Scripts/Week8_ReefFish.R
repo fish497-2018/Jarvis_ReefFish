89ea5a931fbe423e3898503aec6986b0e79becc9
Raegan Jarvis <raeganejarvis@gmail.com>
2018-05-16 20:20:46 -0700

    Tables of fished and not fished species

--- Scripts/Week8_ReefFish.R
+++ Scripts/Week8_ReefFish.R
@@ -9,3 +9,9 @@ library(tidyverse)
 
 reef_fish_bootless <- select(reef_fish, Genus, species, Bootless, Museum.Bootless, Fished)
 View(reef_fish_bootless)
+
+reef_fish_bootless_fished <- filter(reef_fish_bootless, Fished == "fished" | Fished == "Fished")
+View(reef_fish_bootless_fished)
+
+reef_fish_bootless_notfished <- filter(reef_fish_bootless, Fished == "not fished")
+View(reef_fish_bootless_notfished)
